1120b168d425dfb1b2c8e2762b02e2a910ca58d7
Akash552000 <akash654817@gmail.com>
2026-06-24 19:26:57 +0530

    Add files via upload

---
+++ ripple_carry_adder.v
@@ -0,0 +1,24 @@
+
+module ripple_carry_adder (
+    input  wire [3:0] a,
+    input  wire [3:0] b,
+    input  wire       cin,
+    output wire [3:0] sum,
+    output wire       cout
+);
+
+    wire c1, c2, c3;
+
+    assign sum[0] = a[0] ^ b[0] ^ cin;
+    assign c1     = (a[0] & b[0]) | (b[0] & cin) | (a[0] & cin);
+
+    assign sum[1] = a[1] ^ b[1] ^ c1;
+    assign c2     = (a[1] & b[1]) | (b[1] & c1) | (a[1] & c1);
+
+    assign sum[2] = a[2] ^ b[2] ^ c2;
+    assign c3     = (a[2] & b[2]) | (b[2] & c2) | (a[2] & c2);
+
+    assign sum[3] = a[3] ^ b[3] ^ c3;
+    assign cout   = (a[3] & b[3]) | (b[3] & c3) | (a[3] & c3);
+
+endmodule
